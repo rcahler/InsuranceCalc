8cf21c120404d70952994718ae20d932f85f971e
Ryan Ahler <rahler@bsu.edu>
2019-04-04 10:07:08 -0400

    Moved additional portions of calculateInsuranceVeryHigh to separate methods

--- src/InsuranceCalc.java
+++ src/InsuranceCalc.java
@@ -13,7 +13,17 @@ public class InsuranceCalc {
     }
 
     public double calculateInsuranceVeryHigh(double income) {
-        return (income-60000)*0.02+105600;
+        return (income- getAdjustment())* getWeight() + getConstant();
+    }
+    public int getConstant() {
+        return 105600;
+    }
+
+    public double getWeight() {
+        return 0.02;
+    }
+    public int getAdjustment() {
+        return 60000;
     }
 
 }
