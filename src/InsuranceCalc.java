9d65f55f77068e21ee02b7d9f146d4b7a994736e
Ryan Ahler <rahler@bsu.edu>
2019-04-04 10:11:52 -0400

    Moved InsuranceStrategyVeryHigh into its own class

--- src/InsuranceCalc.java
+++ src/InsuranceCalc.java
@@ -1,6 +1,9 @@
 public class InsuranceCalc {
 
     public double calculateInsurance(double income) {
+
+        final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+
         if (income <= 10000) {
             return income * 0.365;
         } else if (income <= 30000) {
@@ -8,22 +11,7 @@ public class InsuranceCalc {
         } else if (income <= 60000) {
             return (income - 30000) * 0.1 + 76500;
         } else {
-            return calculateInsuranceVeryHigh(income);
+            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
         }
     }
-
-    public double calculateInsuranceVeryHigh(double income) {
-        return (income- getAdjustment())* getWeight() + getConstant();
-    }
-    public int getConstant() {
-        return 105600;
-    }
-
-    public double getWeight() {
-        return 0.02;
-    }
-    public int getAdjustment() {
-        return 60000;
-    }
-
 }
